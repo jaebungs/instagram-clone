12b49b8a4eede12a1e52331ceefb20d00eba5319
jaebungs <jaebungs@gmail.com>
2021-05-20 17:19:33 -0400

    express server basic setup

--- server/app.js
+++ server/app.js
@@ -5,3 +5,9 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+    console.log(`server is running on ${port}`)
+})
